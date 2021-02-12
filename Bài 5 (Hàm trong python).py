927e220edc15ca7343cbe0e795676c9535798254
Tran Thanh Binh <tranthanhbinh.thcsthanhson@gmail.com>
2021-02-12 18:11:22 +0700

    Commit ngày 12/2/2021 lần 1

---
+++ Bài 5 (Hàm trong python).py
@@ -0,0 +1,30 @@
+## Example 1:
+# def Print_console():
+#     print('Đang in ra màn hình')
+
+# Print_console()
+
+## Example 2
+# def Print_console(a):
+#     print(a)
+
+# Print_console('Đang in ra màn hình')
+
+##Example 3
+# def Sum(a,b):
+#     return a+b
+
+# k = Sum(1,2)
+# print(k)
+
+##Example 4
+# def Find_Max(*a):
+#     max = a[1]
+#     for item in a:
+#         if item > max:
+#             max = item;
+#     return max
+
+# k = Find_Max(1,2,3,5,2,2,183,38883,8483838)
+# print(k)
+
